24c2a41e3dc51872bb0abfb3afd8e07cc81a1bb1
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-09 20:01:18 +0300

    Add some packages

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Calendar from "./src/components/Calendar";
+import React from "react";
+import CustomLayout from "./src/components/CustomLayout";
+
+export default function App(): JSX.Element {
+  return (
+    <CustomLayout>
+      <Text>selam</Text>
+    </CustomLayout>
+  );
+}
+
