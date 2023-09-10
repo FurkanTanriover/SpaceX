bfaa7fea1d6307e316a8846aa78eedffab430e11
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-10 18:57:37 +0300

    Page layout improved

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
-import { View, Image, StyleSheet, SafeAreaView } from "react-native";
+import { View, Image, StyleSheet, SafeAreaView, Text } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
+import CustomAvatar from "./Avatar";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,7 +11,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <SafeAreaView style={styles.container}>
       <Image source={require("../assets/background.png")} style={styles.backgroundImage} />
-      {children}
+      <View style={styles.welcomeContainer}>
+        <CustomAvatar source="https://i.pravatar.cc/300" />
+        <View className="space-y-2" style={{ display: "flex", flexDirection: "column" }}>
+          <Text style={{ color: "white", fontSize: 16, fontWeight: "bold", marginLeft: wp("5%") }}>Furkan Tanrıöver</Text>
+          <Text style={{ color: "white", fontSize: 12, marginLeft: wp("5%") }}>Welcome to SpaceX Tracker</Text>
+        </View>
+      </View>
+      <View style={styles.content}>{children}</View>
     </SafeAreaView>
   );
 };
@@ -26,10 +34,21 @@ const styles = StyleSheet.create({
     left: 0,
     width: wp("100%"),
     height: hp("100%"),
-    opacity: 1, // İmajın saydamlığı, isteğe bağlı olarak ayarlanabilir
+    opacity: 1,
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
+  },
+  welcomeContainer: {
+    marginTop: hp("2%"),
+    height: hp("12%"),
+    paddingHorizontal: wp("5%"),
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  content: {
+    flex: 1,
+    alignItems: "center",
   },
 });
 
