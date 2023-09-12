ec28277ce05fa5188351af0beb1d62244305f21f
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-12 11:35:38 +0300

    Create ApiManager, Auth and Fetch methods

---
+++ src/api/Auth.ts
@@ -0,0 +1,33 @@
+import ApiManager from "./ApiManager";
+
+export const login = async (email, password) => {
+  try {
+    const response = await ApiManager("/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        email,
+        password,
+      },
+    });
+    return response.data.token;
+  } catch (error) {}
+};
+
+export const register = async (email, password) => {
+    try {
+      const response = await ApiManager("/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          email,
+          password,
+        },
+      });
+      return response.data.token;
+    } catch (error) {}
+  };
---
+++ src/api/Fetch.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import ApiManager from "./ApiManager";
+import { useDispatch } from "react-redux";
+import { loadToken } from "../utils/storage";
+
+export const get = async (url, dispatch, success) => {
+  const token = await loadToken();
+  axios({
+    method: "get",
+    url,
+    headers: {
+      contentType: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+        console.log("GET SUCCESS: => ", url, response.data);
+      dispatch({ type: success, payload: response.data });
+    })
+    .catch((err) => {
+      console.log("GET ERROR: => ", url, err?.response);
+    });
+};
