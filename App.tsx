e1157c3e22fb95b3e56cd9be092ab75ce5463adb
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-12 11:33:55 +0300

    Create redux provider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import AppNavigator from "./src/navigation/App";
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
 
 export default function App(): JSX.Element {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
