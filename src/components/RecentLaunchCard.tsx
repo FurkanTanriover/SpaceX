6c09224461dc9d957e64cf633677e05be7ca8394
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-14 23:21:20 +0300

    Update RecentLaunchCard component

--- src/components/RecentLaunchCard.tsx
+++ src/components/RecentLaunchCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ScrollView } from "react-native";
 import { View, Image, Text, StyleSheet } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 
@@ -15,11 +16,13 @@ const RecentLaunchCard: React.FC<RecentLaunchCardProps> = ({ title, description,
       <View style={styles.imageContainer}>
         <Image source={{ uri: imageURL }} style={styles.image} />
       </View>
-      <View style={styles.detailsContainer}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.description}>{description}</Text>
-        <Text className="text-[12px] text-white/40">{date}</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.description}>{description}</Text>
+          <Text className="text-[12px] text-white/40">{date}</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -28,7 +31,7 @@ const styles = StyleSheet.create({
   card: {
     height: hp("11%"),
     flexDirection: "row",
-    backgroundColor: "rgba(54, 68, 91, 0.5)", 
+    backgroundColor: "rgba(54, 68, 91, 0.5)",
     borderRadius: 5,
     overflow: "hidden",
     padding: 5,
