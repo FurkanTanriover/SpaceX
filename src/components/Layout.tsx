e3c7d8f631d8abb30c82f9499e822cb2833708e5
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-14 23:18:25 +0300

    Update Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,23 @@
 import React, { ReactNode } from "react";
-import { View, Image, StyleSheet, SafeAreaView, Text } from "react-native";
+import { View, Image, StyleSheet, SafeAreaView, Text, Button } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 import CustomAvatar from "./Avatar";
+import { useDispatch } from "react-redux";
+import { deleteToken } from "../redux/action";
+import { useNavigation } from "@react-navigation/native";
+import IconButton from "./IconButton";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const handleLogout = async () => {
+    dispatch(deleteToken());
+    navigation.navigate("Auth" as never); 
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Image source={require("../assets/background.png")} style={styles.backgroundImage} />
@@ -16,6 +26,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <View className="space-y-2" style={{ display: "flex", flexDirection: "column" }}>
           <Text style={{ color: "white", fontSize: 16, fontWeight: "bold", marginLeft: wp("5%") }}>Furkan Tanrıöver</Text>
           <Text style={{ color: "white", fontSize: 12, marginLeft: wp("5%") }}>Welcome to SpaceX Tracker</Text>
+        </View>
+        <View>
+          <IconButton
+            name="logout"
+            onPress={() => {
+              handleLogout();
+            }}
+          />
         </View>
       </View>
       <View style={styles.content}>{children}</View>
@@ -45,6 +63,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: wp("5%"),
     flexDirection: "row",
     alignItems: "center",
+    justifyContent: "space-between",
   },
   content: {
     flex: 1,
