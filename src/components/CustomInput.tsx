3b4de98624ded7654fc1625a5e6a41d00395ead9
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-15 15:39:55 +0300

    Update CustomInput component

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,19 +1,38 @@
 import React from "react";
-import { View, TextInput, StyleSheet, StyleProp, ViewStyle } from "react-native";
-
+import { View, TextInput, StyleSheet, StyleProp, ViewStyle, TouchableOpacity, Image } from "react-native";
+import IconButton from "./IconButton";
 interface CustomInputProps {
   placeholder: string;
-  icon?: string;
   style?: StyleProp<ViewStyle>;
-  value: string; // Yeni eklenen value prop'u
-  onChangeText: (text: string) => void; // Değişiklikleri dinleyecek bir fonksiyon
+  value: string;
+  onChangeText: (text: string) => void;
+  isPassword?: boolean;
 }
 
-const CustomInput: React.FC<CustomInputProps> = ({ placeholder, icon, style, value, onChangeText }) => {
+const CustomInput: React.FC<CustomInputProps> = ({ placeholder, style, value, onChangeText, isPassword }) => {
+  const [isSecure, setIsSecure] = React.useState(isPassword); // İlk başta isPassword prop'unun değerine göre başlat
+
+  const toggleSecureTextEntry = () => {
+    setIsSecure((prevIsSecure) => !prevIsSecure); // Mevcut durumu tersine çevir
+  };
+
   return (
     <View style={[styles.container, style]}>
-      {icon && <></>}
-      <TextInput  value={value} onChangeText={onChangeText} placeholder={placeholder} style={styles.input} placeholderTextColor="#36445B" />
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        style={styles.input}
+        placeholderTextColor="#36445B"
+        secureTextEntry={isSecure} // secureTextEntry özelliğini isSecure'a göre ayarla
+      />
+      {isPassword && (
+        <View>
+          <IconButton name= {isSecure?"eye":"eye-off"} onPress={() => {
+            toggleSecureTextEntry();
+          }} />
+        </View>
+      )}
     </View>
   );
 };
@@ -29,6 +48,8 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 8,
+    width: 24, // Görünür/Görünmez simge boyutunu ayarlayabilirsiniz
+    height: 24,
   },
   input: {
     flex: 1,
